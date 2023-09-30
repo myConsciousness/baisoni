b0c13374773e1d45c0ba09e5cc2f340d2a377bcc
So Asano <so.asano@gmail.com>
2023-10-01 03:42:52 +0900

    日時表示の仕様を変更

--- src/app/_lib/strings/datetime.ts
+++ src/app/_lib/strings/datetime.ts
@@ -1,31 +1,43 @@
 const NOW = 5
 const MINUTE = 60
 const HOUR = MINUTE * 60
-const DAY = HOUR * 24
-const MONTH = DAY * 28
-const YEAR = DAY * 365
+// const DAY = HOUR * 24
+// const MONTH = DAY * 28
+// const YEAR = DAY * 365
 
-export function formattedSimpleDate(dateStr: string, absoluteTimeOnly: boolean = false): string {
-  let ts = Number(new Date(dateStr))
-  const diffSeconds = Math.floor((Date.now() - ts) / 1000)
+export function formattedSimpleDate(dateStr: string, now: Date = new Date(), useRelativeTime: boolean = true): string {
+  const date = new Date(dateStr)
+  const ms = Number(date)
+  const diffSeconds = Math.floor((Number(now) - ms) / 1000)
 
-if (absoluteTimeOnly) {
-    return new Date(ts).toLocaleDateString()
-  } else {
+  if (useRelativeTime) {
     if (diffSeconds < NOW) {
       return `now`
     } else if (diffSeconds < MINUTE) {
       return `${diffSeconds}s`
     } else if (diffSeconds < HOUR) {
       return `${Math.floor(diffSeconds / MINUTE)}m`
-    } else if (diffSeconds < DAY) {
-      return `${Math.floor(diffSeconds / HOUR)}h`
-    } else if (diffSeconds < MONTH) {
-      return `${Math.floor(diffSeconds / DAY)}d`
-    } else if (diffSeconds < YEAR) {
-      return `${Math.floor(diffSeconds / MONTH)}mo`
-    } else {
-      return new Date(ts).toLocaleDateString()
     }
   }
+
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  const hours = `${date.getHours()}`
+  const zeroIfNeeded = date.getMinutes() < 10 ? '0' : ''
+  const minutes = date.getMinutes()
+
+  if (date.toDateString() === now.toDateString()) {
+    return `Today ${hours}:${zeroIfNeeded}${minutes}`
+  }
+
+  const twentyFourHoursAgo = now
+  twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24)
+  
+  if (date.toDateString() === twentyFourHoursAgo.toDateString()) {
+    return `Yesterday ${hours}:${zeroIfNeeded}${minutes}`
+  }
+
+  return `${year}/${month}/${day} ${hours}:${zeroIfNeeded}${minutes}`
 }

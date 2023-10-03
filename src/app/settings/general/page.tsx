d270d64923b8ac97578fdd4c867b37d37f8796de
ば <masaya.kurata@std.code.ac.jp>
2023-10-03 22:42:32 +0900

    とりあえずpush

--- src/app/settings/general/page.tsx
+++ src/app/settings/general/page.tsx
@@ -1,28 +1,18 @@
 'use client';
 import {isMobile} from "react-device-detect";
-
-import {Accordion, AccordionItem, Button, ButtonGroup, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Switch, Select, SelectItem} from "@nextui-org/react";
 import {useEffect, useState} from "react";
+import {
+    Accordion,
+    AccordionItem,
+    Button,
+    ButtonGroup,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownTrigger, Select, SelectItem, Switch
+} from "@nextui-org/react";
 
 export default function Root() {
-    const contentFilteringList = ['Explicit Sexual Images', 'Other Nudity', 'Sexually Suggestive', 'Violent / Bloody', 'Hate Group Iconography', 'Spam', 'Impersonation']
-    /*
-    const [darkMode, setDarkMode] = useState(false);
-    const color = darkMode ? 'dark' : 'light'
-
-    const modeMe = (e:any) => {
-        setDarkMode(!!e.matches);
-    };
-
-    useEffect(() => {
-        console.log('hoge')
-        const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
-
-        setDarkMode(matchMedia.matches);
-        matchMedia.addEventListener("change", modeMe);
-
-        return () => matchMedia.removeEventListener("change", modeMe);
-    }, []);*/
 
     return(
         <>
@@ -70,32 +60,6 @@ export default function Root() {
                                 <SelectItem key={'a'} value={'a'}>a</SelectItem>
                             </Select>
                         </div>
-                    </AccordionItem>
-                    <AccordionItem key="contentFiltering" aria-label="Accordion 1" title="Content Filtering">
-                        {contentFilteringList.map((item, index) => (
-                            <div key={index}
-                                className={'flex justify-between items-center'}
-                            >
-                                <div>{item}</div>
-                                <div className={''}>
-                                    <ButtonGroup>
-                                        <Button>Hide</Button>
-                                        <Button>Warn</Button>
-                                        <Button>Show</Button>
-                                    </ButtonGroup>
-                                </div>
-                            </div>
-                        ))}
-                    </AccordionItem>
-                    <AccordionItem key='mute' aria-label={'Mute'} title='Mute'>
-                        <Accordion>
-                            <AccordionItem key={'muteWords'} aria-label={'Mute Word'} title={'Mute Words'}>
-
-                            </AccordionItem>
-                            <AccordionItem key={'muteAccounts'} aria-label={'Mute Account'} title={'Mute Accounts'}>
-
-                            </AccordionItem>
-                        </Accordion>
                     </AccordionItem>
                 </Accordion>
             </div>
